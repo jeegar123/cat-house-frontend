8c736df113a9db0274d3a368916911af8a4d6009
jeegarlearner <50761184+jeegarlearner@users.noreply.github.com>
2020-05-06 19:30:21 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { CatService } from './service/cat.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
@@ -56,6 +56,7 @@ const routes:Routes=[
     MatCardModule,
     MatMenuModule,
     RouterModule.forRoot(routes)
+    
     
   ],
   providers: [CatService],
